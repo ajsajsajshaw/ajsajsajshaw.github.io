7e8265e831aace8adfb20d62c2d00e0080577acc
ajsajsajshaw <ashaw01@risd.edu>
2021-10-15 13:08:15 -0400

    Create js_ajsajsajshaw.js

---
+++ js_ajsajsajshaw.js
@@ -0,0 +1,10 @@
+// alert("Hello World!");
+console.log("Hello avery website looks great!");
+
+$(document).ready(function(){
+    $("button").click(function()
+    {
+    $("p").toggle();
+  });
+});
+    
