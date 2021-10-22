e88503157cde88f3b266a15e587a2900349ff2bf
ajsajsajshaw <ashaw01@risd.edu>
2021-10-22 15:51:51 -0400

    Update js_ajsajsajshaw.js

--- js_ajsajsajshaw.js
+++ js_ajsajsajshaw.js
@@ -1,27 +1,9 @@
-alert("Hello World!");
+// alert("Hello World!");
 console.log("Hello avery website looks great!");
 
 $(function(){
 
   $('#darkmodebutton').click(function(){
-    $(this).toggle();
-  }):
+    $('#iconcontainer').toggle();
+  });
 })
-
-
-
-// $(document).ready(function(){
-//     $("darkmodebutton").click(function(){
-//       $(this).toggle();
-
-//       // if ($('button:contains("hide exercises")')){
-//       //   $("p").toggle();
-//       //   $(this).html('show exercise');
-      
-      
-
-//     });
-// });
-    
-//       // $("p").toggle();
-//       // $("p").toggleClass('on');
