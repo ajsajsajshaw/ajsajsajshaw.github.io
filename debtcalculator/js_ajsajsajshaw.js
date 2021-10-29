ff8b05c002d985e2f18e176fefb6dddcd7dc9849
ajsajsajshaw <ashaw01@risd.edu>
2021-10-29 12:55:48 -0400

    Create js_ajsajsajshaw.js

---
+++ debtcalculator/js_ajsajsajshaw.js
@@ -0,0 +1,14 @@
+// alert("Hello World!");
+console.log("Hello avery website looks great!");
+
+$(document).ready(function(){
+$("#darkmodebutton").click(function(){
+  $('html').toggleClass('darkmode');
+
+});
+
+})
+
+  // $('#darkmodebutton').click(function(){
+  //   $('#iconcontainer').toggle();
+  // });
